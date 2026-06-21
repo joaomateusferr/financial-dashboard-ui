4187b0875845695bfbe8865fa1a561246f164efc
Joao Ferreira <joaomateusferr@gmail.com>
2026-06-21 19:44:44 -0300

    Add page not found page

---
+++ screens/page-not-found.php
@@ -0,0 +1,10 @@
+<section class="page-shell">
+
+    <div class="page-card">
+
+        <h1 class="page-title">404</h1>
+        <p class="page-description">Page not found.</p>
+
+    </div>
+
+</section>
