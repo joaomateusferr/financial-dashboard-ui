a0889980406f1d66c84ed906936f79131dd27496
Joao Ferreira <joaomateusferr@gmail.com>
2026-07-09 20:29:49 -0300

    Change signin result page

--- screens/signin-result.php
+++ screens/signin-result.php
@@ -21,12 +21,12 @@ if($Success){
 
 <section class="page-shell">
 
-    <div class="page-card text-center">
+    <div class="page-card">
 
         <h1 class="page-title"><?php echo $Title; ?></h1>
         <p class="page-description"><?php echo $Description; ?></p>
         <div class="page-footer">
-            <button class="btn-primary" onclick="window.location.href='/'">Home</button>
+            Do you want to log in? <a href="<?php echo "/";?>">Login</a>
         </div>
 
     </div>
